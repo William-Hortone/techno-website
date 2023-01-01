c7e2b3db90c6e560bbdd0b5a3ecdc841148f90f7
william Hortone <willhortone@gmail.com>
2023-01-01 20:24:16 +0800

    added two component, brand and whatgpt3

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as Article} from './article/Article';
 export { default as Brand} from './brand/Brand';
 export { default as CTA} from './cta/CTA';
 export { default as Navbar} from './navbar/Navbar';
-// export { defauft as Feature} from './feature/Feature';
+export { default as Feature} from './feature/Feature';
--- src/container/whatGPT3/Whatgpt3.jsx
+++ src/container/whatGPT3/Whatgpt3.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Feature } from '../../components'
+import './whatgpt3.css';
+
+
 
 function Whatgpt3() {
   return (
-    <div>
-      Whatgpt3
+    <div className="gpt3__whatgpt3 section__margin" id='whatgpt3'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title='What is GPT-3' text='loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora adipisci aliquam aliquid aut reprehenderit. Facere at laudantium quam unde dolor!' />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className='gradient__text'>The possibility are beyond your imagination</h1>
+        <p>EXplore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title=' Chatbot' text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora adipisci aliquam aliquid aut reprehenderit. Facere at laudantium quam unde dolor!'/>
+        <Feature title=' knowledgebase' text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora adipisci aliquam aliquid aut reprehenderit. Facere at laudantium quam unde dolor!'/>
+        <Feature title=' Chatbot' text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora adipisci aliquam aliquid aut reprehenderit. Facere at laudantium quam unde dolor!'/>
+      </div>
     </div>
   )
 }
